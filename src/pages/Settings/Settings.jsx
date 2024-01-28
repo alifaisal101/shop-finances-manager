cfd3ff62e4120c4d4c5610a86233ea8d75fc79ab
Ali <alifais1122@gmail.com>
2024-01-28 17:30:41 +0300

    added toggle button to the settings page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,12 +1,35 @@
 import { IonPage } from '@ionic/react';
 import './__Settings.css';
 import Header from '../../global/Header/Header';
+import React from 'react';
+import {
+  IonContent,
+  IonItem,
+  IonList,
+  IonListHeader,
+  IonToggle,
+} from '@ionic/react';
 
-const Settings = () => {
+const Settings = (props) => {
+  // Add or remove the "dark" class on the document body
+
   return (
     <IonPage>
       <Header title="اﻹعدادات" />
-      <h1>settings</h1>
+      <IonContent>
+        <IonListHeader>Appearance</IonListHeader>
+        <IonList inset={true}>
+          <IonItem>
+            <IonToggle
+              checked={props.theme == 'dark'}
+              onIonChange={props.toggleTheme}
+              justify="space-between"
+            >
+              Dark Mode
+            </IonToggle>
+          </IonItem>
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 };
