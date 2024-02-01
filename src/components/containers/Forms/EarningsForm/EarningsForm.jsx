81d25751985b2f192e73077bc66b9781afb6c944
Ali <alifais1122@gmail.com>
2024-02-02 02:32:53 +0300

    added earnings form

--- src/components/containers/Forms/EarningsForm/EarningsForm.jsx
+++ src/components/containers/Forms/EarningsForm/EarningsForm.jsx
@@ -1,13 +1,55 @@
+import {
+  IonButton,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonInput,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+} from '@ionic/react';
+
 import './__EarningsForm.css';
 import { useLocation } from 'react-router-dom';
+import { displayDate } from '../../../../util/display.functions';
+import { useState } from 'react';
 
 const EarningsForm = () => {
   const earningId = useLocation();
-  console.log(earningId);
 
+  const [earning, setEarning] = useState({
+    date: new Date(),
+    amount: null,
+  });
   return (
-    <div className="earnings-form-container">
-      <h1>sad</h1>
+    <div className="earnings-form-container form">
+      <IonGrid>
+        <IonRow className="form_row">
+          <IonCol>
+            <IonInput
+              label="اﻹيراد"
+              labelPlacement="floating"
+              placeholder="أدخل اﻹيراد الكلي"
+              className="form_input"
+              type="number"
+              step="1000"
+              max={1000000000}
+              min={0}
+            ></IonInput>
+          </IonCol>
+          <IonCol>
+            <IonInput
+              label="التاريخ"
+              labelPlacement="stacked"
+              placeholder="أدخل تاريخ"
+              type="date"
+              value={displayDate(new Date())}
+              className="form_input"
+            ></IonInput>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
     </div>
   );
 };
