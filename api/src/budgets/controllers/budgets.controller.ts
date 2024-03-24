631b36fdc1460e64cb740c5290905fce16fb48bc
Ali <alifais1122@gmail.com>
2024-03-24 14:13:35 +0300

    added budget controller and services

--- api/src/budgets/controllers/budgets.controller.ts
+++ api/src/budgets/controllers/budgets.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { BudgetsService } from '../services/budgets.service';
+import { FetchRecordsDto } from 'src/dtos/fetch-records.dto';
+import { isMongoId } from 'class-validator';
+import { Types } from 'mongoose';
 
 @Controller('budgets')
-export class BudgetsController {}
+export class BudgetsController {
+  constructor(private budgetSrv: BudgetsService) {}
+
+  @Post('fetch-budgets')
+  async fetchBudgets(@Body() body: FetchRecordsDto) {
+    return await this.budgetSrv.fetchBudgets(body);
+  }
+
+  @Delete('delete-budget/:budgetId')
+  async deleteBudget(@Param('budgetId') budgetId: string) {
+    try {
+      if (!budgetId || !isMongoId(budgetId)) {
+        throw new Error('Invalid BudgetId');
+      }
+    } catch (err) {
+      throw new BadRequestException(err);
+    }
+
+    return await this.budgetSrv.deleteBudget(new Types.ObjectId(budgetId));
+  }
+}
--- api/src/budgets/services/budgets.service.ts
+++ api/src/budgets/services/budgets.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Budget, BudgetDocument } from '../entites/budget.entity';
+import { Model, Types } from 'mongoose';
+import { FetchRecordsDto } from 'src/dtos/fetch-records.dto';
 
 @Injectable()
-export class BudgetsService {}
+export class BudgetsService {
+  constructor(
+    @InjectModel(Budget.name) private budgetModel: Model<BudgetDocument>,
+  ) {}
+
+  async fetchBudgets(options: FetchRecordsDto) {
+    const page = options.page || 1;
+    const recordsPerPage = options.recordsPerPage || 10;
+    const skip = (page - 1) * recordsPerPage;
+
+    try {
+      return await this.budgetModel.find().skip(skip).limit(recordsPerPage);
+    } catch (err) {
+      throw new InternalServerErrorException(err, 'Failed to fetch budgets.');
+    }
+  }
+
+  async deleteBudget(budgetId: Types.ObjectId) {
+    try {
+      return await this.budgetModel.findByIdAndDelete(budgetId);
+    } catch (err) {
+      throw new InternalServerErrorException(
+        err,
+        'Failed to delete the budget.',
+      );
+    }
+  }
+}
