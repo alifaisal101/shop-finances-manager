2fc1cee7186f0d9c20c5afd2f51630219ca4aa37
Ali <alifais1122@gmail.com>
2024-03-24 14:00:07 +0300

    added earnings controller

--- api/src/earnings/controllers/earnings.controller.ts
+++ api/src/earnings/controllers/earnings.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Types } from 'mongoose';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { EarningsService } from '../services/earnings.service';
+import { FetchRecordsDto } from 'src/dtos/fetch-records.dto';
+import { PostEarningsDto } from '../dtos/req/post-earnings.dto';
+import { PatchEarningsDto } from '../dtos/req/patch-earnings.dto';
+import { isMongoId } from 'class-validator';
 
 @Controller('earnings')
-export class EarningsController {}
+export class EarningsController {
+  constructor(private earningSrv: EarningsService) {}
+
+  @Post('fetch-earnings')
+  async fetchEarnings(@Body() body: FetchRecordsDto) {
+    return await this.earningSrv.findAll(body);
+  }
+
+  @Post('add-earning')
+  async addEarning(@Body() body: PostEarningsDto) {
+    return await this.earningSrv.create(body);
+  }
+
+  @Patch('modify-earning')
+  async modifyEarning(@Body() body: PatchEarningsDto) {
+    return await this.earningSrv.update(body);
+  }
+
+  @Delete('delete-earning')
+  async deleteEarning(@Param() earningId: string) {
+    try {
+      if (!earningId || !isMongoId(earningId)) {
+        throw new Error('Invalid EarningId');
+      }
+    } catch (err) {
+      throw new BadRequestException(err);
+    }
+
+    return await this.earningSrv.delete(new Types.ObjectId(earningId));
+  }
+}
