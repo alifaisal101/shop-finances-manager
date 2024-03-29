673862269cdde566a78ecdcaa0b4cbd2b6172dd9
Ali <alifais1122@gmail.com>
2024-03-29 19:23:32 +0300

    added modify otherspending service

--- api/src/other-spendings/services/other-spendings.service.ts
+++ api/src/other-spendings/services/other-spendings.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import {
   OtherSpending,
@@ -18,6 +22,17 @@ export class OtherSpendingsService {
     private otherSpendingModel: Model<OtherSpendingDocument>,
     private transactionsSrv: TransactionsService,
   ) {}
+
+  async findById(otherSpendingId: Types.ObjectId) {
+    try {
+      return await this.otherSpendingModel.findById(otherSpendingId);
+    } catch (err) {
+      throw new InternalServerErrorException(
+        err,
+        'Failed to find otherSpending.',
+      );
+    }
+  }
 
   async findAll(options: FetchRecordsDto) {
     const page = options.page || 1;
@@ -104,7 +119,103 @@ export class OtherSpendingsService {
     return { spending, spendingTransactions };
   }
 
-  async modifySpending() {}
+  async modifySpending(newOtherSpending: PatchOtherSpendingDto) {
+    const otherSpending = await this.findById(newOtherSpending.otherSpendingId);
+    if (otherSpending) {
+      throw new NotFoundException('No otherSpending with that Id was found.');
+    }
+
+    let addedDebt = 0;
+    let addedPaidTo = 0;
+
+    // New Transactions
+    if (newOtherSpending?.newTransactions?.length > 0) {
+      const newOtherSpendingTransactions = newOtherSpending.newTransactions.map(
+        (transaction) => {
+          addedPaidTo += transaction.amount;
+          return {
+            ...transaction,
+            section: 'otherSpending',
+            type: 'expense',
+          };
+        },
+      );
+
+      const addTransactionResults = await this.transactionsSrv.addTransactions(
+        newOtherSpendingTransactions,
+      );
+      const addedTransactionsIds = addTransactionResults.transactionsResult.map(
+        (t) => t._id,
+      );
+      otherSpending.transactions.concat(addedTransactionsIds);
+    }
+
+    // Modified Transactions
+    if (newOtherSpending?.modifiedTransactions?.length > 0) {
+      const modifiedTransactionsResult = [];
+
+      for (let i = 0; i < newOtherSpending.modifiedTransactions.length; i++) {
+        const modifiedTransaction = newOtherSpending.modifiedTransactions[i];
+        const transactionDb = await this.transactionsSrv.findById(
+          modifiedTransaction.transactionId,
+        );
+        addedPaidTo = modifiedTransaction.amount - transactionDb.amount;
+        modifiedTransactionsResult.push(
+          await this.transactionsSrv.modifyTransaction(modifiedTransaction),
+        );
+      }
+    }
+
+    // Removed Transactions
+    if (newOtherSpending?.removedTransactions?.length > 0) {
+      const removedTransactionsIds = [];
+
+      for (let i = 0; i < newOtherSpending.removedTransactions.length; i++) {
+        const transactionToRemoveId = newOtherSpending.removedTransactions[i];
+        const transactionDb = await this.transactionsSrv.findById(
+          transactionToRemoveId,
+        );
+
+        removedTransactionsIds.push(transactionToRemoveId);
+        addedPaidTo -= transactionDb.amount;
+      }
+      const removedTransactionsResult =
+        await this.transactionsSrv.removeTransactions(
+          newOtherSpending.removedTransactions,
+        );
+
+      otherSpending.transactions = otherSpending.transactions.filter(
+        (transactionId) => !removedTransactionsIds.includes(transactionId),
+      );
+    }
+
+    if (newOtherSpending.price) {
+      addedDebt = newOtherSpending.price - otherSpending.price;
+      otherSpending.price = newOtherSpending.price;
+    }
+
+    if (newOtherSpending.title) {
+      otherSpending.title = newOtherSpending.title;
+    }
+
+    if (newOtherSpending.paymentType) {
+      otherSpending.paymentType = newOtherSpending.paymentType;
+    }
+
+    if (newOtherSpending.spendingDate) {
+      otherSpending.spendingDate = newOtherSpending.spendingDate;
+    }
+
+    otherSpending.debt += addedDebt;
+    otherSpending.paidTo += addedPaidTo;
+    otherSpending.updatedAt = new Date();
+
+    try {
+      return await otherSpending.save();
+    } catch (err) {
+      throw new Error('Failed to modify otherSpending.');
+    }
+  }
 
   async removeSpending(
     spendingId: Types.ObjectId,
