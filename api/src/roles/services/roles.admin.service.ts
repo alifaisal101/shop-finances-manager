2090408c07dbc14dfa5b2da38c0faeed33afe33e
Ali <alifais1122@gmail.com>
2024-05-27 22:54:21 +0300

    added remove all service for roles

--- api/src/roles/services/roles.admin.service.ts
+++ api/src/roles/services/roles.admin.service.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { FetchRolesDto } from '../dtos/req/fetch-roles.dto';
 import { RemoveRolesDto } from '../dtos/req/remove-roles.dto';
+import { UsersService } from 'src/users/services/users.service';
+import { badRequestExceptionCatch } from 'src/utils/functions/error';
 
 @Injectable()
 export class RolesAdminService {
-  constructor(private rolesSrv: RolesService) {}
+  constructor(
+    private rolesSrv: RolesService,
+    private usersSrv: UsersService,
+  ) {}
 
   async fetchRoles(body: FetchRolesDto) {
     const skip = ((body.page | 1) - 1) * (body.recordsPerPage | 5);
@@ -28,8 +33,24 @@ export class RolesAdminService {
   }
 
   async removeRoles(body: RemoveRolesDto) {
-    for (let i = 0; i < body.rolesIds.length; i++) {
-      const roleId = body.rolesIds[i];
+    try {
+      const users = await this.usersSrv.find(
+        { roleId: { $in: body.rolesIds } },
+        { _id: 1, username: 1 },
+        false,
+        0,
+        0,
+        true,
+      );
+      if (users.length > 0) {
+        throw new Error(
+          "Can't delete a role, if there are users attached to it.",
+        );
+      }
+
+      return await this.rolesSrv.removeMany(body.rolesIds);
+    } catch (err) {
+      throw badRequestExceptionCatch(err);
     }
   }
 }
